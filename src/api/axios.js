1098653ceee1f438e27b73149640f40baab73408
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-11 20:40:27 +0300

    updateApi

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://localhost:7112/api',
+    baseURL: 'https://guleb23-ordersusersapi-ff19.twc1.net/api',
     headers: {
         'Content-Type': 'application/json',
     },
